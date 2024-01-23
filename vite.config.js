613027a6c38c9a06373a884abc13488700dd239a
Sylvain Blanc <sylvain.blanc@dawex.com>
2024-01-23 21:22:36 +0100

    move to docs

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
